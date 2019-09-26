619aeea71b18c11924619d96a2ec00ae6dbe4e4d
Dennis Walsh <lawjolla@aol.com>
2019-09-25 20:27:14 -0700

    Initial commit

---
+++ getScreenshot.js
@@ -0,0 +1,127 @@
+const chrome = require(`chrome-aws-lambda`);
+const puppeteer = require(`puppeteer-core`);
+
+
+const preparePageForTests = async (page) => {
+  // Pass the User-Agent Test.
+  const userAgent =
+        `Mozilla/5.0 (X11; Linux x86_64)` +
+        `AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36`
+  await page.setUserAgent(userAgent)
+
+  // Pass the Webdriver Test.
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, `webdriver`, {
+      get: () => false
+    })
+  })
+
+  // Pass the Chrome Test.
+  await page.evaluateOnNewDocument(() => {
+    // We can mock this in as much depth as we need for the test.
+    // @ts-ignore
+    window.navigator.chrome = {
+      runtime: {}
+      // etc.
+    }
+  })
+
+  // Pass the Permissions Test.
+  await page.evaluateOnNewDocument(() => {
+    const originalQuery = window.navigator.permissions.query
+    // @ts-ignore
+    return (window.navigator.permissions.query = (parameters: any) =>
+      (parameters.name === `notifications`
+        ? Promise.resolve({ state: Notification.permission })
+        : originalQuery(parameters)))
+  })
+
+  // Pass the Plugins Length Test.
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, `plugins`, {
+      // This just needs to have `length > 0` for the current test,
+      // but we could mock the plugins too if necessary.
+      get: () => [1, 2, 3, 4, 5]
+    })
+  })
+
+  // Pass the Languages Test.
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, `languages`, {
+      get: () => [`en-US`, `en`]
+    })
+  })
+}
+
+const args = [
+  `--no-sandbox`,
+  `--disable-setuid-sandbox`,
+  `--disable-infobars`,
+  `--window-position=0,0`,
+  `--ignore-certifcate-errors`,
+  `--ignore-certifcate-errors-spki-list`,
+  `--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"`
+]
+
+
+const getScreenshot = async (url, element) => {
+  const options = {
+    args,
+    ignoreHTTPSErrors: true,
+    userDataDir: `./tmp`,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  }
+  try {
+    console.log(url)
+    const browser = await puppeteer.launch(options)
+    const page = await browser.newPage()
+    await preparePageForTests(page)
+    await page.setViewport({ width: 1000, height: 1000 })
+    await page.goto(url, { waitUntil: `networkidle0` })
+    await page.waitForSelector(element)
+    await page.waitForSelector(`img`)
+    const area = await page.$(element)
+    if (area) {
+      const classNames = await area
+        .getProperty(`className`)
+        .then(cn => cn.jsonValue())
+        .then(classNameString => classNameString.split(` `))
+      if (classNames.includes(`isIframe`)) {
+        await page.waitForSelector(`iframe`)
+        await page.waitFor(2000)
+        const frame = await page.frames().find(f => f.url().includes(`youtube`))
+        if (frame) {
+          console.log(`waiting`)
+          await frame.waitForSelector(`.ytp-cued-thumbnail-overlay`)
+          await page.waitFor(2000)
+        }
+      }
+      // await page.waitFor(10000)
+      const clip = await area.boundingBox()
+      if (clip) {
+        const screenshot = await page.screenshot({
+          clip
+        })
+        await browser.close()
+        return screenshot
+      }
+
+      await browser.close()
+      return null
+    }
+    // const img = await page.evaluate(() => {
+    //   document.querySelector(element)
+    // })
+    await browser.close()
+    return null
+  } catch (e) {
+    console.log(e)
+  }
+  return null
+}
+
+
+module.exports = { getScreenshot };
---
+++ screenshot.js
@@ -0,0 +1,28 @@
+const { parse } = require(`url`);
+const { getScreenshot } = require(`./chromium`);
+
+function getUrlFromPath(str) {
+  const url = str.slice(1);
+  if (!url.startsWith(`http`)) {
+    return `https://${url}`;
+  }
+  return url;
+}
+
+const screenshot = async (req, res) => {
+  try {
+    const { element, url } = req.query
+
+    const file = await getScreenshot(url, element);
+    res.statusCode = 200;
+    res.setHeader(`Content-Type`, `image/${type}`);
+    res.end(file);
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader(`Content-Type`, `text/html`);
+    res.end(`<h1>Server Error</h1><p>Sorry, there was a problem</p>`);
+    console.error(e.message);
+  }
+};
+
+module.exports = screenshot
