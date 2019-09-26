e1f1241fa0631d95b1730c3744a1e0689c62a048
Dennis Walsh <lawjolla@aol.com>
2019-09-26 11:07:16 -0700

    Update error handling

--- getScreenshot.js
+++ getScreenshot.js
@@ -62,7 +62,9 @@ const args = [
   `--window-position=0,0`,
   `--ignore-certifcate-errors`,
   `--ignore-certifcate-errors-spki-list`,
-  `--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"`
+  `--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"`,
+    `--disable-software-rasterizer`,
+    `--disable-gpu`
 ]
 
 
